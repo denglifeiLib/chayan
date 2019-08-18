e4c300251421a5c1df00833983e4fffb560edd08
denglifei <denglifly3507@163.com>
2019-08-19 00:34:11 +0800

    add bang

---
+++ src/components/Range_table.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="range_table">
+      <table>
+          <thead>
+              <tr>
+                  <td v-for="(columnEl,k) in column" :key="k" :width="columnEl.width">{{columnEl.title}}</td>
+              </tr>
+          </thead>
+          <tbody>
+              <tr v-for="(row,k) in rows" :key="k">
+                <td v-for="(columnEl,j) in column" :key="j" :class="row.range? 'self':''">
+                      <!-- {{[columnEl.key]}} -->
+                      <span v-if="columnEl.key!=='name'">
+                          {{!row[columnEl.key] && columnEl.key === 'range' ? k+1 : row[columnEl.key]}}
+                      </span>
+                      <div v-if="columnEl.key==='name'" class="name_column">
+                          <img :src="row.header" alt="" class="header">
+                          <p class="name">{{row.name}}</p>
+                          <p class="dis">{{row.office}}</p>
+                      </div>
+                </td>
+              </tr>
+          </tbody>
+      </table>
+  </div>
+</template>
+
+<script>
+import Icon from 'vue-svg-icon/Icon.vue';
+import Quota from './Quota';
+export default {
+  name: 'baogao_card',
+  data() {
+      return {
+          
+      }
+  },
+  props: {
+    rows: {
+        type: Array,
+        detault: ()=>[]
+    },
+    column: {
+        type: Array,
+        detault: ()=>[]
+    }
+  },
+  components: {Icon, Quota}
+}
+</script>
+
+<style scoped lang="less">
+.range_table{
+    width: 100%;
+    table{width: 100%;}
+    thead td{
+        height: 34px;
+        line-height: 34px;
+        text-align: center;
+        height:34px;
+        background:rgba(249,250,252,1);
+        font-size:13px;
+font-weight:500;
+color:rgba(168,168,168,1);
+    }
+    tbody td{
+        padding: 10px 20px;
+        vertical-align: middle;
+        text-align: center;
+        font-size:13px;
+        font-weight:500;
+        color:rgba(51,51,51,1);
+        &.self{
+            background: #FFFBEF;
+        }
+        &:last-child{
+            font-size: 14px;
+        }
+    }
+    tbody tr:nth-child(1) td:first-child{
+        background: url(../assets/images/range_1.png) center center no-repeat;
+        background-size: 24px 24px;
+        text-indent: -10000px;
+    }
+    tbody tr:nth-child(2) td:first-child{
+        background: url(../assets/images/range_2.png) center center no-repeat;
+        background-size: 24px 24px;
+        text-indent: -10000px;
+    }
+    tbody tr:nth-child(3) td:first-child{
+        background: url(../assets/images/range_3.png) center center no-repeat;
+        background-size: 24px 24px;
+        text-indent: -10000px;
+    }
+    .name_column{
+        text-align: left;
+        overflow: hidden;
+        .header{
+            float: left;
+            width:40px;
+            height:40px;
+            margin-right: 13px;
+            border-radius: 100%;
+        }
+        .dis{
+            font-size:12px;
+            font-weight:400;
+            color:rgba(168,168,168,1);
+        }
+    }
+}
+</style>
--- src/views/home_bang.vue
+++ src/views/home_bang.vue
@@ -1,41 +1,16 @@
 <template>
     <div class="entry_page">
-        <img :src="require('@/assets/images/index_banner.jpg')" alt="" class="banner block">
-        <wv-flex>
-            <wv-flex-item class="fdf">
-                <div class="top_type top_type_1">
-                    <p class="type_tt">人工智能面诊<icon name="card_icon-2" scale="1.8" class="model_icon"></icon></p>
-                    <p class="type_dis"><icon name="card_icon-2" scale="1.8" class="model_icon"></icon>本周排行 20</p>
-                </div>
-            </wv-flex-item>
-            <wv-flex-item class="fdf">
-                <div class="top_type top_type_2">
-                    <p class="type_tt">人工智能面诊<icon name="card_icon-2" scale="1.8" class="model_icon"></icon></p>
-                    <p class="type_dis"><icon name="card_icon-2" scale="1.8" class="model_icon"></icon>完成40%</p>
-                </div>
-            </wv-flex-item>
-        </wv-flex>
-
-        <wv-group class="no_border item_title">
-            <wv-cell title="云镜贴吧" value="进入贴吧" is-link to="/">
-                <img :src="require('@/assets/images/home_icon_tb@2x.png')" slot="icon" alt="" class="hd_icon">
-            </wv-cell>
-        </wv-group>
-
-        <div class="tie_item" v-for="(item,k) in ties" :key="k">
-            <div class="header_name">
-                <img :src="item.header" alt="" class="header">
-                <span class="name">{{item.name}}</span>
-            </div>
-            <p class="title">{{item.title}}</p>
-            <p class="text_ellipse">{{item.content}}</p>
-            <p class="support">{{item.support}} <span class="pl8">赞同</span> </p> 
-        </div>
+        <wv-tabs :line-width="18" sticky>
+            <wv-tab v-for="(item,k) in tabs" :title="item.val" :key="k">
+                <range_table :rows="k===0 ?rows1 : rows2" :column="k===0 ?column1:column2"></range_table>
+            </wv-tab>
+        </wv-tabs>
     </div>
 </template>
 
 <script>
 
+import Range_table from '@/components/Range_table';
 import Icon from 'vue-svg-icon/Icon.vue';
 import * as Axios from '@/utils/Action';
 
@@ -43,32 +18,141 @@ export default {
     name: 'test',
     data() {
         return {
-            text: '222',
-            ties: [
+            tabs: [
+                {
+                    key: 'baogao',
+                    val: '检测榜'
+                }, {
+                    key: 'beizhu',
+                    val: '学习榜'
+                }
+            ],
+            column1: [
+                {
+                    key: 'range',
+                    title: '排名',
+                    width: '20%'
+                }, {
+                    key: 'name',
+                    title: '业务员',
+                    width: '40%'
+                }, {
+                    key: 'result',
+                    title: '本周检测次数',
+                    width: '40%'
+                }
+            ],
+            column2: [
+                {
+                    key: 'range',
+                    title: '排名',
+                    width: '20%'
+                }, {
+                    key: 'name',
+                    title: '业务员',
+                    width: '40%'
+                }, {
+                    key: 'result',
+                    title: '完成百分比',
+                    width: '40%'
+                }
+            ],
+            rows1: [
                 {
                     header: require('@/assets/images/header.jpg'),
-                    name: '锺嫱嫱',
-                    title: '如何面谈三人或多人的客户',
-                    content:  '有时面谈在客户的办公室会遇上客户的访客，这时就变成了三人或多人，那此时千有时面谈在客户的办公室会遇上客户的访客，这时就变成了三人或多人，那此时千万不要',
-                    support: '10K'
+                    name: '钱冬冬',
+                    office: '河南分公司',
+                    result: '2123'
                 }, {
                     header: require('@/assets/images/header.jpg'),
-                    name: '锺嫱嫱',
-                    title: '如何面谈三人或多人的客户',
-                    content:  '有时面谈在客户的办公室会遇上客户的访客，这时就变成了三人或多人，那此时千有时面谈在客户的办公室会遇上客户的访客，这时就变成了三人或多人，那此时千万不要',
-                    support: '10K'
+                    name: '钱冬冬',
+                    office: '河南分公司',
+                    result: '2123'
                 }, {
                     header: require('@/assets/images/header.jpg'),
-                    name: '锺嫱嫱',
-                    title: '如何面谈三人或多人的客户',
-                    content:  '有时面谈在客户的办公室会遇上客户的访客，这时就变成了三人或多人，那此时千有时面谈在客户的办公室会遇上客户的访客，这时就变成了三人或多人，那此时千万不要',
-                    support: '10K'
+                    name: '钱冬冬',
+                    office: '河南分公司',
+                    result: '2123'
                 }, {
                     header: require('@/assets/images/header.jpg'),
-                    name: '锺嫱嫱',
-                    title: '如何面谈三人或多人的客户',
-                    content:  '有时面谈在客户的办公室会遇上客户的访客，这时就变成了三人或多人，那此时千有时面谈在客户的办公室会遇上客户的访客，这时就变成了三人或多人，那此时千万不要',
-                    support: '10K'
+                    name: '钱冬冬',
+                    office: '河南分公司',
+                    result: '2123'
+                },{
+                    header: require('@/assets/images/header.jpg'),
+                    name: '钱冬冬',
+                    office: '河南分公司',
+                    result: '2123'
+                }, {
+                    header: require('@/assets/images/header.jpg'),
+                    name: '钱冬冬',
+                    office: '河南分公司',
+                    result: '2123'
+                }, {
+                    header: require('@/assets/images/header.jpg'),
+                    name: '钱冬冬',
+                    office: '河南分公司',
+                    result: '2123'
+                }, {
+                    header: require('@/assets/images/header.jpg'),
+                    name: '钱冬冬',
+                    office: '河南分公司',
+                    result: '2123'
+                }, {
+                    range: '55',
+                    header: require('@/assets/images/header.jpg'),
+                    name: '本人',
+                    office: '河南分公司',
+                    result: '2123'
+                }
+            ],
+            rows2: [
+                {
+                    header: require('@/assets/images/header.jpg'),
+                    name: '钱冬冬',
+                    office: '河南分公司',
+                    result: '100%'
+                }, {
+                    header: require('@/assets/images/header.jpg'),
+                    name: '钱冬冬',
+                    office: '河南分公司',
+                    result: '100%'
+                }, {
+                    header: require('@/assets/images/header.jpg'),
+                    name: '钱冬冬',
+                    office: '河南分公司',
+                    result: '100%'
+                }, {
+                    header: require('@/assets/images/header.jpg'),
+                    name: '钱冬冬',
+                    office: '河南分公司',
+                    result: '99%'
+                },{
+                    header: require('@/assets/images/header.jpg'),
+                    name: '钱冬冬',
+                    office: '河南分公司',
+                    result: '78%'
+                }, {
+                    header: require('@/assets/images/header.jpg'),
+                    name: '钱冬冬',
+                    office: '河南分公司',
+                    result: '75%'
+                }, {
+                    header: require('@/assets/images/header.jpg'),
+                    name: '钱冬冬',
+                    office: '河南分公司',
+                    result: '74%'
+                }, {
+                    header: require('@/assets/images/header.jpg'),
+                    name: '钱冬冬',
+                    office: '河南分公司',
+                    result: '66%'
+                }, {
+                    range: '55',
+                    header: require('@/assets/images/header.jpg'),
+                    name: '本人',
+                    office: '河南分公司',
+                    result: '55%'
                 }
             ]
         }
@@ -82,7 +166,7 @@ export default {
         this.$emit('tabbar', {
             show: true,
             active: 1
-        })
+        });
     },
     methods: {
         sendRequest() {
@@ -93,77 +177,10 @@ export default {
             })
         }
     },
-    components: {Icon}
+    components: {Icon, Range_table}
 }
 </script>
 
 <style lang="less">
-.banner{
-    margin: 0 auto;
-}
-.top_type{
-    height: 215px;
-    margin-top: 8px;
-    text-align: center;
-    background-position: top center;
-    background-repeat: no-repeat;
-    background-size: 100% 100%;
-    &_1{
-        color: #fff;
-        margin-left: 8px;
-        background-image: url('../assets/images/sy_bg_1@2x.png');
-    }
-    &_2{
-        color: #1D77DE;
-        margin-right: 8px;
-        background-image: url('../assets/images/sy_bg_2@2x.png');
-    }
-    .type_tt{
-        font-size: 16px;
-        padding-top: 135px;
-        font-weight: bold;
-    }
-    .type_dis{
-        padding-top: 8px;
-        font-size: 13px;
-    }
-}
-.item_title{
-    margin-bottom: 15px;
-}
-.item_title .weui-cells{
-    .weui-cell_hd{
-        padding-right: 8px;
-        .hd_icon{
-            width: 27px;
-        }
-    }
-    .weui-cell__bd{
-        font-size: 18px;
-        font-weight: bold;
-    }
-    .weui-cell__ft{
-        font-size: 12px;
-    }
-}
-.tie_item{
-    padding: 18px 15px;
-    margin: 0 12px 24px;
-    background:rgba(255,255,255,1);
-    box-shadow:0px 1px 8px 0px rgba(0,0,0,0.15);
-    border-radius:4px;
-    background: url('../assets/images/home_bg_tb180@2x.png') bottom center no-repeat;
-    background-size: 100% auto;
-    .title{
-        font-size:16px;
-        padding: 8px 0;
-        font-weight:500;
-    }
-    .support{
-        padding: 8px 0;
-        font-size:12px;
-        font-weight:500;
-        color:rgba(168,168,168,1);
-    }
-}
+
 </style>
